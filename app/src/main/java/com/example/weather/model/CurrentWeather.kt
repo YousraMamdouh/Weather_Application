3015cdd8de41d98889079a033518b310bd21e848
YousraMamdouh <yousramamdouh98@gmail.com>
2023-03-24 00:32:02 +0200

    api pojo

---
+++ app/src/main/java/com/example/weather/model/CurrentWeather.kt
@@ -0,0 +1,13 @@
+package com.example.weather.model
+
+import com.google.gson.annotations.SerializedName
+
+ data class CurrentWeather
+     (val timezone:Long,val id:Long,val name:String,val cod:Long){
+
+
+ }
+
+
+
+
